ea10edd83f14244ae49940f5f5f6ae863f5f25ff
DerM4209 <57956243+DerM4209@users.noreply.github.com>
2021-12-16 05:44:57 +0100

    Add files via upload

--- Marlin-2.0.x/Marlin/src/feature/ecm_edm_mode.h
+++ Marlin-2.0.x/Marlin/src/feature/ecm_edm_mode.h
@@ -10,26 +10,16 @@ xyz_pos_t origin_position;
 xyz_pos_t start_position;
 xyz_pos_t target_position;
 
-
 #ifdef ECM_EDM_MODE
-
 
 void move_to_target(){
 idle();
-const millis_t ms = millis();
-static millis_t message_event_ms = 0;
-
- if (ELAPSED(ms, message_event_ms)){
-message_event_ms = ms + 2000;
-SERIAL_ECHO_MSG(STR_BUSY_PROCESSING);
-}
-
 if (ecm_edm_voltage > TARGET_VOLTAGE && reverse == false){
 destination = target_position;
 prepare_line_to_destination();
 SERIAL_ECHOLNPGM("towards target", ecm_edm_voltage);
  }
-if (ecm_edm_voltage < TARGET_VOLTAGE){
+if (ecm_edm_voltage < TARGET_VOLTAGE && reverse == false){
 planner.quick_stop();
 planner.synchronize();
 reverse = true;
@@ -44,58 +34,14 @@ reverse = false;
 }
 }
 
-/*
-
-
-
-
-
-if (ecm_edm_voltage > GAP_LOWER_LIMIT && ecm_edm_voltage < GAP_UPPER_LIMIT){
-quickstop_stepper();
-}
-}
-*/
-
- /*
-do_blocking_move_to(target_position, feedrate_mm_s);
-if (ecm_edm_voltage < GAP_UPPER_LIMIT && ecm_edm_voltage > GAP_LOWER_LIMIT){
-quickstop_stepper();
-}
-}
-}
-*/
-
-
-
-/*if (ecm_edm_voltage > GAP_LOWER_LIMIT && ecm_edm_voltage < GAP_UPPER_LIMIT){
-SERIAL_ECHOLNPGM("Gap OK!", ecm_edm_voltage);
-destination = current_position;
-prepare_line_to_destination();
-if (destination != current_position){
-quickstop_stepper();
-}
-}
-*/
-/*if (current_position == target_position){
-SERIAL_ECHOLNPGM("target reached!");	
-const millis_t wait = millis() + 500;
-while (PENDING(millis(), wait)) idle();
-start_position = target_position;
-SERIAL_ECHOLNPGM("position secured, continue!");
-}*/
-
-
-
 void ecm_edm_mode(){
-
 if (edm_ecm_setup == true){
 origin_position = current_position;
 start_position = current_position;
 target_position = current_position;
 edm_ecm_setup = false;
 }
-
-if (new_destination != target_position && new_destination != origin_position){ //if entered position is different from target position and not the origin position
+if (new_destination != target_position && new_destination != origin_position){
 target_position = new_destination;
 target_reached = false;
 SERIAL_ECHOLNPGM("new target saved!");
@@ -104,6 +50,38 @@ move_to_target();
 }
 }
 }
-
-
   #endif
+  
+/*
+if (ecm_edm_voltage > GAP_LOWER_LIMIT && ecm_edm_voltage < GAP_UPPER_LIMIT){
+quickstop_stepper();
+}
+}
+//const millis_t ms = millis();
+//static millis_t message_event_ms = 0;
+ //if (ELAPSED(ms, message_event_ms)){
+//message_event_ms = ms + 2000;
+//SERIAL_ECHO_MSG(STR_BUSY_PROCESSING);
+//}
+ /*
+do_blocking_move_to(target_position, feedrate_mm_s);
+if (ecm_edm_voltage < GAP_UPPER_LIMIT && ecm_edm_voltage > GAP_LOWER_LIMIT){
+quickstop_stepper();
+}
+}
+}
+/*if (ecm_edm_voltage > GAP_LOWER_LIMIT && ecm_edm_voltage < GAP_UPPER_LIMIT){
+SERIAL_ECHOLNPGM("Gap OK!", ecm_edm_voltage);
+destination = current_position;
+prepare_line_to_destination();
+if (destination != current_position){
+quickstop_stepper();
+}
+}
+/*if (current_position == target_position){
+SERIAL_ECHOLNPGM("target reached!");	
+const millis_t wait = millis() + 500;
+while (PENDING(millis(), wait)) idle();
+start_position = target_position;
+SERIAL_ECHOLNPGM("position secured, continue!");
+}*/
